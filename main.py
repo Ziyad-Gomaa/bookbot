aceb09aa8a6d315e8c827f3dd0a6578cf682c25a
Ziyad-Gomaa <ziyadd.gomaa@gmail.com>
2025-09-07 13:48:05 +0300

    add dict_sort, include only alphabetical characters, and print a pretty report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, char_count
+from stats import word_count, char_count, dict_sort
 
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
@@ -6,19 +6,25 @@ def get_book_text(path_to_file):
 
     return contents
 
-
-
 def main():
     path_to_file = "./books/frankenstein.txt"
     text = get_book_text(path_to_file)
 
     num_of_words = word_count(text)
-    
-    print(f"{num_of_words} words found in the document")
+    dict_of_chars = char_count(text)
+    chars_sorted = dict_sort(dict_of_chars)
 
-    chars_dict = char_count(text)
+    print(12 * "=", "BOOKBOT", "=" * 12)
+    print("Analyzing book found at books/frankenstein.txt...")
 
-    print(chars_dict)
+    print(11 * "-", "Word Count", "-" * 11)
+    print(f"Found {num_of_words} total words")
+
+    print(9 * "-", "Character Count", "-" * 9)
+    for char in chars_sorted:
+        print(char["char"] + ":", char["num"])
+
+    print(13 * "=", "END", "=" * 13)
 
 
 main()
--- stats.py
+++ stats.py
@@ -5,9 +5,23 @@ def char_count(contents):
     char_stats = {}
 
     for char in contents.lower():
-        if char in char_stats:
-            char_stats[char] += 1
-        else:
-            char_stats[char] = 1
-    
+        if char.isalpha():
+            if char in char_stats:
+                char_stats[char] += 1
+            else:
+                char_stats[char] = 1
+
     return char_stats
+
+def dict_sort(dict_of_chars):
+    sorted_list = []
+
+    for char in dict_of_chars:
+        sorted_list.append({"char": char, "num": dict_of_chars[char]})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
+
+def sort_on(list_of_dicts):
+        return list_of_dicts["num"]
